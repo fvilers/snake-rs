4695c6b95a999fd632b22d2c6114b02e33013a47
Fabian Vilers <fabian.vilers@dev-one.com>
2024-03-07 09:37:14 +0100

    Add crossterm

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+use crossterm::{
+    cursor::{self, SetCursorStyle},
+    event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
+    execute, queue, style,
+    terminal::{self, ClearType},
+};
+use std::io::{Result, Write};
+
+pub fn run<W>(w: &mut W) -> Result<()>
+where
+    W: Write,
+{
+    execute!(w, terminal::EnterAlternateScreen)?;
+    terminal::enable_raw_mode()?;
+
+    loop {
+        queue!(
+            w,
+            style::ResetColor,
+            terminal::Clear(ClearType::All),
+            cursor::Hide,
+            cursor::MoveTo(1, 1),
+            style::Print("Hello, world! Press 'q' to exit.")
+        )?;
+        w.flush()?;
+
+        if read_char()? == 'q' {
+            execute!(w, SetCursorStyle::DefaultUserShape)?;
+            break;
+        };
+    }
+
+    execute!(
+        w,
+        style::ResetColor,
+        cursor::Show,
+        terminal::LeaveAlternateScreen
+    )?;
+    terminal::disable_raw_mode()
+}
+
+fn read_char() -> Result<char> {
+    loop {
+        if let Ok(Event::Key(KeyEvent {
+            code: KeyCode::Char(c),
+            kind: KeyEventKind::Press,
+            modifiers: _,
+            state: _,
+        })) = event::read()
+        {
+            return Ok(c);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use snake_rs::run;
+use std::{io, process};
+
 fn main() {
-    println!("Hello, world!");
+    let mut stdout = io::stdout();
+
+    if let Err(e) = run(&mut stdout) {
+        eprintln!("{e}");
+        process::exit(-1);
+    }
 }
